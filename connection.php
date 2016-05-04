2e64700ed6d1521e80a686e017a92df0f2e0fd9d
1510215 <d.kannan@rgu.ac.uk>
2016-05-04 11:24:28 +0100

    php

---
+++ connection.php
@@ -0,0 +1,10 @@
+<?php
+
+define('DB_SERVER', 'ap-cdbr-azure-east-c.cloudapp.net');
+define('DB_USERNAME', 'b91a8a491e3026');
+define('DB_PASSWORD', '7fd7a00b');
+define('DB_DATABASE', 'CMM007ALDB-1510215');
+$db = mysqli_connect(DB_SERVER,DB_USERNAME,DB_PASSWORD,DB_DATABASE);
+
+
+?>
---
+++ index.php
@@ -0,0 +1,117 @@
+<?php
+
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<!-- - [START OF HEAD] ============================================================================================= -->
+<head>
+    <meta charset="UTF-8">
+
+    <title>Home</title>
+
+    <!-- - CSS Stylesheet- -->
+    <link rel="stylesheet" href="assets/css/css.css" type="text/css">
+    <link href='https://fonts.googleapis.com/css?family=Passion+One' rel='stylesheet' type='text/css'>
+
+</head>
+<!-- - [END OF HEAD] ============================================================================================= -->
+
+
+<!-- - [START OF BODY] ============================================================================================= -->
+<body>
+
+<!-- (START OF HEADER) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -- - -- - - - -->
+<header>
+    <section class="container" id="header">
+
+
+        <section class="headercontainer">
+            <h1>myBlog</h1>
+            <h3>because the internet needs to know what I think</h3>
+        </section>
+
+
+
+        <!-- (START OF NAV) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+        <section class="headercontainer" id="nav-index">
+
+            <nav>
+
+                <ul>
+                    <li><a href="blog.php">All Blog Items</a> </li>
+                    <li><a href="blog.php">Work Blog Items</a> </li>
+                    <li><a href="blog.php">University Blog Items</a> </li>
+                    <li><a href="blog.php">Family Blog Items</a> </li>
+                    <li><a href="add.php">Insert Blog Items</a> </li>
+                </ul>
+
+            </nav>
+
+        </section>
+
+    </section>
+    <!-- (END OF NAV) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+</header>
+<!-- (END OF HEADER) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -  - - - -- - -->
+
+
+
+<!-- (START OF MAIN) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+<main>
+
+    <section class="container" id="main">
+
+
+        <section class="container-content" id="content-index">
+
+            <section class="content">
+
+                <p>
+                    Suspendisse non lacinia lectus. Fusce pharetra eleifend velit, a vulputate sapien eleifend et.
+                    In vehicula tempor est, vitae fringilla metus laoreet ut.
+                    Fusce iaculis eu diam congue tempus.
+                    Cras consequat mi id eros consequat, sed accumsan ipsum facilisis. Cras semper, lorem et eleifend placerat,
+                    lorem ex varius eros, at scelerisque lectus lacus nec risus.
+                </p>
+
+            </section>
+
+        </section>
+
+
+        <!-- (START OF LOGO) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+        <section class="container-content" id="image-index">
+
+            <img id="logo" src="assets/images/blog.png">
+
+        </section>
+        <!-- (END OF LOGO) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+    </section>
+
+</main>
+<!-- (END OF MAIN) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+
+
+<!-- (START OF FOOTER) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+<footer>
+
+    <section class="container" id="footer">
+
+        <p>
+            Designed by Dineshan Kannan, 2016
+        </p>
+
+    </section>
+
+</footer>
+<!-- (END OF FOOTER) - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -->
+
+</body>
+<!-- - [END OF BODY] ============================================================================================= -->
+</html>
